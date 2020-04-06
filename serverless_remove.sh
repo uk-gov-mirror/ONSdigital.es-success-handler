13ad23dc071d2dd3717a1edfab1bf49e1d554f0a
Michael Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-04-06 16:04:13 +0100

    Update serverless_remove.sh

--- serverless_remove.sh
+++ serverless_remove.sh
@@ -2,6 +2,7 @@
 
 cd success-handler-repository
 echo Installing dependancies
+serverless plugin install --name serverless-pseudo-parameters
 serverless plugin install --name serverless-latest-layer-version
 echo Destroying serverless bundle...
 serverless remove --verbose;
