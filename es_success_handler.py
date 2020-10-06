aa15253b794fc4b29dfa34247987cb6407510470
saulpountney <saulpountney1994@gmail.com>
2020-10-06 11:54:22 +0100

    made changes to the method, encountered issues with the testing.

--- es_success_handler.py
+++ es_success_handler.py
@@ -1,16 +1,20 @@
 import json
 
-from es_aws_functions import exception_classes, general_functions
+from es_aws_functions import exception_classes, general_functions, aws_functions
 
 
 def lambda_handler(event, context):
     logger = general_functions.get_logger()
     error_message = ''
     current_module = "Success Handler."
+
+    bpm_queue_url = None
+
     run_id = 0
     try:
         logger.info("Success Handler Begun.")
         run_id = event['RuntimeVariables']["run_id"]
+        bpm_queue_url = event['RuntimeVariables']["bpm_queue_url"]
 
         outcome = "PASS"
 
@@ -19,12 +23,18 @@ def lambda_handler(event, context):
         jsonresponse = json.loads(jsonresponse)
     except Exception as e:
         error_message = general_functions.handle_exception(e, current_module,
-                                                           run_id, context)
+                                                           run_id, context=context,
+                                                           bpm_queue_url=bpm_queue_url)
     finally:
         if (len(error_message)) > 0:
             logger.error(error_message)
             raise exception_classes.LambdaFailure(error_message)
 
     logger.info("Successfully completed module: " + current_module)
+
+    # Send end status to BPM.
+    status = "RUN COMPLETE"
+    current_module = "BMI Results Complete"
+    aws_functions.send_bpm_status(bpm_queue_url, current_module, status, run_id)
 
     return jsonresponse
--- tests/test_success_handler.py
+++ tests/test_success_handler.py
@@ -1,10 +1,11 @@
 import pytest
 from es_aws_functions import test_generic_library
-
+from unittest import mock
 import es_success_handler as lambda_wrangler_function
 
 runtime_variables = {
-              "RuntimeVariables": {"run_id": "01201"},
+              "RuntimeVariables": {"run_id": "01201",
+                                   "bpm_queue_url": "fake_bpm_queue_url"},
               }
 
 ##########################################################################################
@@ -20,7 +21,8 @@ runtime_variables = {
          None, "es_success_handler.json.loads",
          "'Exception'", test_generic_library.wrangler_assert)
     ])
-def test_general_error(which_lambda, which_runtime_variables,
+@mock.patch('lambda_wrangler_function.aws_functions.send_bpm_status')
+def test_general_error(send_bpm_status, which_lambda, which_runtime_variables,
                        which_environment_variables, mockable_function,
                        expected_message, assertion):
     test_generic_library.general_error(which_lambda, which_runtime_variables,
@@ -34,6 +36,7 @@ def test_general_error(which_lambda, which_runtime_variables,
         (lambda_wrangler_function, None,
          "KeyError", test_generic_library.wrangler_assert)
     ])
+
 def test_key_error(which_lambda, which_environment_variables,
                    expected_message, assertion):
     test_generic_library.key_error(which_lambda, which_environment_variables,
